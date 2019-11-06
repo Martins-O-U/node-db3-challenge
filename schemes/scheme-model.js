335ecebc9e3251779c145aa788328e7d9a226a95
Martins-O-U <martinsonyedikachi@gmail.com>
2019-11-06 17:01:35 +0100

    Created an update and post function for the database

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,28 @@ const db = require('../data/db-config')
 
 module.exports ={
     find,
+    findById,
+    add,
+    update
 }
 
 function find (){
     return db("schemes");
 }
+
+function findById(id){
+    return db('schemes').where({id}).first()
+}
+
+function add (scheme) {
+    return db("schemes").insert(scheme)
+        .then(id =>{
+            return findById(id[0]);
+        })
+}
+
+function update(changedScheme, id) {
+    return db('schemes')
+      .where({ id })
+      .update(changedScheme);
+  }
